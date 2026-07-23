f3e41d54c897269c658ce998a144e0c37874abb0
Shri-272 <shrikant.mankar272@gmail.com>
2026-07-23 21:00:39 +0530

    Fix typo in welcome message output

--- helloworld.java
+++ helloworld.java
@@ -1,5 +1,5 @@
 public class helloworld {
     public static void main(String[] args) {
-        System.out.println("welcome to java session by clouddevopshub with Chandan, Chndar , abhinav we are ruunig CI by abhinav , pradeep 2nd commit ok");
+        System.out.println("welcome to java session by clouddevopshub with Chandan, Chndar , abhinav we are ruunig CI by abhinav , pradeep 2nd commit ok done");
     }
 }
